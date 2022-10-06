61100084e3051065f4bf951664dd23525613b22b
Robert Eichner <roberteichner3@web.de>
2022-10-06 15:50:27 +0200

    switch to blazor

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace roberteichner.github.io.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
